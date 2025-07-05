acc761fb0217fd479e9f433a4ec179c6285e60d4
andrelencart <andre.lencart@gmail.com>
2025-07-05 16:32:01 +0100

    add changes

--- Backend/database_handlers/db.py
+++ Backend/database_handlers/db.py
@@ -1,8 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
+load_dotenv()
 
-DATABASE_URL = "mysql+mysqlconnector://root:Root123%40@db/User_db"
+user = "root"
+password = os.getenv("MYSQL_ROOT_PASSWORD")
+host = "db"
+database = os.getenv("MYSQL_DATABASE")
+
+DATABASE_URL = f"mysql+mysqlconnector://{user}:{password}@{host}/{database}"
 
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(bind=engine)
